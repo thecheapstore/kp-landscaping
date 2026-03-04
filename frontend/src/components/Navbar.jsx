a5a2473710296e34db1c6575fe0dac87d13359d1
thecheapstore <jackwinchester943@gmail.com>
2026-03-03 22:20:49 -0500

    Logo Update, again!

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,15 +9,15 @@ const Navbar = () => {
   return (
     <nav className="bg-white shadow-md sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-20">
-          {/* Logo */}
-          <Link to="/" className="flex items-center">
-  <img
-    src="/logo.png"
-    alt="KP Landscaping"
-    className="h-21 w-auto object-contain"
-  />
-</Link>
+       <div className="flex justify-between items-center h-16">
+  <Link to="/" className="flex items-center">
+    <img
+      src="/logo.png"
+      alt="KP Landscaping"
+      className="h-12 w-auto"
+    />
+  </Link>
+</div>
 
           {/* Desktop Navigation */}
           <div className="hidden md:flex items-center space-x-8">
